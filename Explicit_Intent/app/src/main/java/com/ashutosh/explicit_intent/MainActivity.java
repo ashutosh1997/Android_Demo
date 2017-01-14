0e2f8ee9dc8bd0561e770900aa926e29b43a1b4d
ASHUTOSH <kumar.ashutosh920@gmail.com>
2017-01-15 04:01:38 +0530

    Done

--- Explicit_Intent/app/src/main/java/com/ashutosh/explicit_intent/MainActivity.java
+++ Explicit_Intent/app/src/main/java/com/ashutosh/explicit_intent/MainActivity.java
@@ -1,13 +1,36 @@
 package com.ashutosh.explicit_intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn = (Button) findViewById(R.id.button);
+    EditText editText = (EditText) findViewById(R.id.editText);
+    EditText editTextNum = (EditText) findViewById(R.id.editTextNum);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+
+                Bundle b = new Bundle();
+                b.putString("name",editText.getText().toString() );
+                b.putInt("roll", Integer.parseInt(editTextNum.getText().toString()));
+
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Explicit_Intent/app/src/main/java/com/ashutosh/explicit_intent/SecondActivity.java
+++ Explicit_Intent/app/src/main/java/com/ashutosh/explicit_intent/SecondActivity.java
@@ -1,9 +1,40 @@
 package com.ashutosh.explicit_intent;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by ASHUTOSH on 15-Jan-17.
  */
 
-public class SecondActivity {
+public class SecondActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        TextView tw = (TextView) findViewById(R.id.textView);
+        StringBuilder msg = new StringBuilder("");
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        if(b.containsKey("name"))
+        {
+            String name = b.getString("name");
+            msg.append("Name : ").append(name);
+        }
+        if (b.containsKey("roll"))
+        {
+            int roll = b.getInt("roll");
+            msg.append("\nRoll : ").append(roll);
+        }
+
+        tw.setText(msg);
+
+    }
 
 }
